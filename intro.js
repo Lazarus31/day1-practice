d4504a714bd0476adbf1ebbe57d1fdf0ebc8aeab
Lazarus1_3 <eshetealazar11@gmail.com>
2026-07-21 17:00:47 +0300

    ay yedai sera

--- intro.js
+++ intro.js
@@ -2,3 +2,4 @@
 console.log(
   "What i am expecting is to be a developer that knows what he is doing. If i saw a problem after this course i am thinking how do i solve and how to make it a real that people can see it and use it.And build my confidence be well ready for my job journey i mean interview Thanks.",
 );
+console.log("ayy dagi weuuuuu");
